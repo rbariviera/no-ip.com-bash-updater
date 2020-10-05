94ab4d692cb74ea202b0d7b32bec0007113173e8
Rodrigo Bariviera <rodrigo@bariviera.net>
2020-10-05 10:24:10 -0300

    Update noipupdater.sh
    change from wget to curl

--- noipupdater.sh
+++ noipupdater.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $(dirname $0);
+
 # No-IP uses emails as usernames, so make sure that you encode the @ as %40
 USERNAME=username
 PASSWORD=password
@@ -13,7 +15,7 @@ USERAGENT="Simple Bash No-IP Updater/0.4 antoniocs@gmail.com"
 mkdir -p $LOGDIR
 mkdir -p $CONFIGDIR
 
-RESULT=$(wget -O "-" --user-agent="$USERAGENT" --no-check-certificate "https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST")
+RESULT=$(curl -isS -u "${USERNAME}:${PASSWORD}" -H "user-agent: ${USERAGENT}" "http://dynupdate.no-ip.com/nic/update?hostname=${HOST}")
 LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
 
 echo $LOGLINE >> $LOGFILE
